570e947b3f44fcc13d513032d77c0612282accd1
deathcoreTur <deathcore.tur@gmail.com>
2018-10-30 19:40:33 +0200

    simple rewrite...

--- grep.py
+++ grep.py
@@ -2,8 +2,7 @@ import re
 
 
 # Check on exist grep and .txt in line
-def validate():
-    global args
+def validate(args):
     command_line = False
     # It should be 'grep'
     first = args[0]
@@ -15,12 +14,12 @@ def validate():
     elif not last.endswith('.txt'):
         msg = "Not find txt file:\n"
     else:
-        return True
+        return True, args
 
     while not command_line:
         command_line = input(msg)
     args = command_line.split()
-    return False
+    return False, args
 
 
 def find(file, args):
@@ -38,6 +37,15 @@ def find(file, args):
                 print(line.rstrip())
 
 
+# For big file where strings are very long
+def read_in_chunks(file_object, chunk_size=1024):
+    while True:
+        data = file_object.read(chunk_size)
+        if not data:
+            break
+        yield data
+
+
 if __name__ == '__main__':
     command_line = False
     while not command_line:
@@ -45,10 +53,10 @@ if __name__ == '__main__':
 
     args = command_line.split()
 
-    right_command = validate()
+    right_command, args = validate(args)
 
     while not right_command:
-        right_command = validate()
+        right_command, args = validate(args)
 
     # Deleted 'grep' from list and get filename. As result, args list consists only necessary parameters
     del args[0]
@@ -56,6 +64,8 @@ if __name__ == '__main__':
     file = open(filename)
 
     if args:
+        # I don't use yield for reading, because the file is line-based,
+        # the file object is already a lazy generator of lines:
         find(file, args)
     else:
         print('You not input mask, restart program with requirements ')
